68032ef3d376aa75b167950a732710a6a79c0484
TharunAnne <tharun.anne@dizitiveit.com>
2024-06-07 18:02:58 +0530

    dZsdfsd

--- lib/src/mock/profile_mock.dart
+++ lib/src/mock/profile_mock.dart
@@ -3,7 +3,7 @@ import '../features/settings/domain/profile.dart';
 final profileMocked = Profile(
   fullName: "Anne Tharun",
   email: "tharun744h@gmail.com",
-  phoneNumber: "+91 9603566744",
+  phoneNumber: "+919603566744",
   city: "Kakinada",
   country: "India",
 );
